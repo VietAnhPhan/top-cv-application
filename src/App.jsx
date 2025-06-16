61e4d65c46074db93c6f10014e6c315183401540
Anh Phan <vietanhphan2810@gmail.com>
2025-06-16 07:47:16 +0000

    updated UI

--- src/App.jsx
+++ src/App.jsx
@@ -81,17 +81,40 @@ function App() {
       </form>
 
     <div className="cv-information">
-      <p>Name: {clickIndex===1 && resume.name}</p>
-      <p>Email: {clickIndex===1 && resume.email}</p>
-      <p>Phone: {clickIndex===1 && resume.phone}</p>
-      <p>School name: {clickIndex===1 && resume.school}</p>
-      <p>Title of study: {clickIndex===1 && resume.study}</p>
-      <p>Date of study: {clickIndex===1 && resume.dateStudy}</p>
-      <p>Company name: {clickIndex===1 && resume.company}</p>
-      <p>Position title: {clickIndex===1 && resume.position}</p>
-      <p>Main responsibilities: {clickIndex===1 && resume.mainResponsibilities}</p>
-      <p>Date from: {clickIndex===1 && resume.dateFrom}</p>
-      <p>Date until: {clickIndex===1 && resume.dateUntil}</p>
+      <header>
+        <h1>{resume.name==='' && 'Your name'} {clickIndex===1 && resume.name}</h1>
+        <p>{resume.email==='' && 'youremail@example.com'} {clickIndex===1 && resume.email}</p>
+        <p>{resume.phone==='' && '(+84) 011 088 095'} {clickIndex===1 && resume.phone}</p>
+      </header>
+      <div className="cv-title-grid">
+        <h2>Education</h2>  
+        <div className="seperator"><hr /></div>
+      </div>
+
+      <div className="cv-content-grid">
+        <p className='content-title'>School name: </p> <p className='content-details'>{resume.school==='' && 'University of Science'} {clickIndex===1 && resume.school}</p>
+        <p className='content-title'>Title of study: </p> <p className='content-details'>{resume.study==='' && 'Computer Science'} {clickIndex===1 && resume.study}</p>
+        <p className='content-title'>Date of study: </p> <p className='content-details'>{resume.dateStudy ==='' && '01/01/1999'} {clickIndex===1 && resume.dateStudy}</p>
+      </div> 
+
+
+        <div className="cv-title-grid">
+        <h2>Work Experience</h2>  
+        <div className="seperator"><hr /></div>
+      </div>
+
+      <div className="cv-content-grid">
+        <p className='content-title'>Company name:</p> <p className='content-details'>{resume.company ==='' && 'Microsoft Corporation'} {clickIndex===1 && resume.company}</p>
+        <p className='content-title'>Position title: </p> <p className='content-details'>{resume.position ==='' && 'Senior Software Developer'} {clickIndex===1 && resume.position}</p>
+        <p className='content-title'>Main responsibilities:</p> <p className='content-details'>{resume.mainResponsibilities ==='' && 'Analyse features, lead a junior memebers, reviewing code, maintain and deploy'} {clickIndex===1 && resume.mainResponsibilities}</p>
+        <p className='content-title'>Date from: </p> <p className='content-details'>{ resume.dateFrom==='' && '01/01/2004' } {clickIndex===1 && resume.dateFrom}</p>
+        <p className='content-title'>Date until: </p> <p className='content-details'>
+          {resume.dateUntil ==='' && '01/01/2014'}
+          {clickIndex===1 && resume.dateUntil}
+        
+        </p>
+      </div>
+
     </div>
 
     </div>
